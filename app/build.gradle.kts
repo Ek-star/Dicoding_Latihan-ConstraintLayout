5479c2daca1eb37cd5ffeba2be8700246fe59d4f
Ek-star <totoekalma@gmail.com>
2023-11-15 19:39:29 +0700

    Fiks Sudah Selesai

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.latihanconstraintlayout"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.latihanconstraintlayout"
